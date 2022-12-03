6260f02f5c2b5064c548ddf7415631c8569ebdc9
Diocechen <78402906+Diocechen@users.noreply.github.com>
2022-12-03 13:55:48 +0800

    Add files via upload

---
+++ Class3.java
@@ -0,0 +1,12 @@
+package myJava14;
+import java.util.*;
+
+public class Class3 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String in = sc.nextLine();
+		System.out.println(in.toUpperCase());
+	}
+
+}
